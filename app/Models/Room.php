3754ef900c3d32d6a70ef7daa59287032f345a61
sadikjj1 <islam15-5550@diu.edu.bd>
2025-07-19 01:47:11 +0600

    role model created

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'room_title',
+        'image',
+        'description',
+        'price',
+        'wifi',
+        'room_type',
+    ];
 }
