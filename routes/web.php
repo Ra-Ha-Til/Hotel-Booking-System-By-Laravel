1f828c281928a0dd66be470c92e83280a564bd25
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-18 20:22:59 +0600

    home index added

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
-
 
 
 route::get('/home',[AdminController::class,'index'])-> name('home');
