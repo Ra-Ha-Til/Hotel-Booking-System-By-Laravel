9e6846b2cbd480f827e6a25dbf62f7a20bb94156
sadikjj1 <islam15-5550@diu.edu.bd>
2025-07-19 00:45:56 +0600

    Admin panel setting

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -237,9 +237,10 @@
         <nav id="sidebar">
             <!-- Sidebar Header-->
             <div class="sidebar-header d-flex align-items-center">
-                <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
+                <div class="avatar"><img src="admin/img/Rahatil.png" alt="..." class="img-fluid rounded-circle">
+                </div>
                 <div class="title">
-                    <h1 class="h5">Mark Stephen</h1>
+                    <h1 class="h5">Md Rahatil</h1>
                     <p>Web Designer</p>
                 </div>
             </div>
