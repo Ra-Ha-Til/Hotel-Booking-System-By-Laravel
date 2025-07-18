264552ed546b6d26f568a05577aaafcbf66dd7f5
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-18 20:31:12 +0600

    Admin controller to route

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -33,4 +33,9 @@ class AdminController extends Controller
         }
 
     }
+
+    public function home()
+    {
+        return view('home.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('home.index');
-});
+
+route::get('/', [AdminController::class, 'home']);
 
 
 route::get('/home',[AdminController::class,'index'])-> name('home');
