c21380f0bd8c0130ce8255eda28a322439b9ff4a
sadikjj1 <islam15-5550@diu.edu.bd>
2025-07-19 02:55:29 +0600

    update

--- resources/views/admin/create_room.blade.php
+++ resources/views/admin/create_room.blade.php
@@ -7,75 +7,41 @@
 
 <body>
     @include('admin.header')
-    <div class="d-flex align-items-stretch">
-
-        <!-- Sidebar Navigation-->
-        @include('admin.sidebar')
-        <!-- Sidebar Navigation end-->
-
-        <div class="page-content">
-            <div class="page-header">
-                <div class="container-fluid">
+    <!-- Sidebar Navigation-->
+    @include('admin.sidebar')
+    <!-- Sidebar Navigation end-->
 
 
-                    <div>
-                        <form>
-                            <div>
-                                <label>Room Title</label>
-                                <input type="text" name="title">
-                            </div>
-                            <div>
-                                <label>Description</label>
 
-                                <textarea name="description"></textarea>
-                            </div>
-                            <div>
-                                <label>Price</label>
-                                <input type="number" name="price">
-                            </div>
-                            <div>
-                                <label>Room Type</label>
-
-                                <select name="type">
-
-                                    <option selected value="regular"> Regular</option>
-                                    <option value="premium"> Premium</option>
-                                    <option value="deluxe"> Deluxe</option>
-
-                                </select>
-                            </div>
-
-                            <div>
-                                <label>Free Wifi</label>
-
-                                <select name="type">
-
-                                    <option selected value="yes"> Yes</option>
-                                    <option value="no"> No</option>
-
-                                </select>
-                            </div>
-
-                            <div>
-                                <label>Upload Image</label>
-                                <input type="file" name="image">
-                            </div>
-
-                            <div>
-                                <input class="btn btn-primary" type="submit" value="Add Room">
-                            </div>
-
-                        </form>
-                    </div>
+    <div class="page-content">
+        <div class="page-header">
+            <div class="container-fluid">
 
 
-                </div>
+
+
             </div>
         </div>
+    </div>
 
-        <!-- footer-->
-        @include('admin.footer')
-        <!-- end footer-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- footer-->
+    @include('admin.footer')
+    <!-- end footer-->
 </body>
 
 </html>
