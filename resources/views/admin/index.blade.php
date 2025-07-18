16b1767f996cbfcc656410adfce55f3455b96e2c
sadikjj1 <islam15-5550@diu.edu.bd>
2025-07-19 00:58:18 +0600

    modify

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,11 +8,18 @@
 <body>
     @include('admin.header')
     <div class="d-flex align-items-stretch">
+
         <!-- Sidebar Navigation-->
         @include('admin.sidebar')
         <!-- Sidebar Navigation end-->
+
+        <!-- body-->
         @include('admin.body')
+        <!-- end body-->
+
+        <!-- footer-->
         @include('admin.footer')
+        <!-- end footer-->
 </body>
 
 </html>
