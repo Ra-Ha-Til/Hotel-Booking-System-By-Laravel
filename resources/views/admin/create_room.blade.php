d13829185480de81c5d451b92f3026d721aebe60
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-19 03:36:53 +0600

    create_room added

--- resources/views/admin/create_room.blade.php
+++ resources/views/admin/create_room.blade.php
@@ -15,6 +15,67 @@
 
 
 
+
+        <div class="page-content">
+            <div class="page-header">
+                <div class="container-fluid">
+
+                    <div>
+                        <form>
+                            <div>
+                                <label>Room Title</label>
+                                <input type="text" name="title">
+                            </div>
+                            <div>
+                                <label>Description</label>
+
+                                <textarea name="description"></textarea>
+                            </div>
+                            <div>
+                                <label>Price</label>
+                                <input type="number" name="price">
+                            </div>
+                            <div>
+                                <label>Room Type</label>
+
+                                <select name="type">
+
+                                    <option selected value="regular"> Regular</option>
+                                    <option value="premium"> Premium</option>
+                                    <option value="deluxe"> Deluxe</option>
+
+                                </select>
+                            </div>
+
+                            <div>
+                                <label>Free Wifi</label>
+
+                                <select name="type">
+
+                                    <option selected value="yes"> Yes</option>
+                                    <option value="no"> No</option>
+
+                                </select>
+                            </div>
+
+                            <div>
+                                <label>Upload Image</label>
+                                <input type="file" name="image">
+                            </div>
+
+                            <div>
+                                <input class="btn btn-primary" type="submit" value="Add Room">
+                            </div>
+
+                        </form>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+
         <!-- footer-->
         @include('admin.footer')
         <!-- end footer-->
