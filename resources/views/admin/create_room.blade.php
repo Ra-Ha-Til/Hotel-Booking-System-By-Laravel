fcdb35b0d7ec91856f7c672adcd1e087353ea75a
sadikjj1 <islam15-5550@diu.edu.bd>
2025-07-19 02:58:43 +0600

    update

--- resources/views/admin/create_room.blade.php
+++ resources/views/admin/create_room.blade.php
@@ -1,47 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    @include('admin.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
 <body>
-    @include('admin.header')
-    <!-- Sidebar Navigation-->
-    @include('admin.sidebar')
-    <!-- Sidebar Navigation end-->
 
-
-
-    <div class="page-content">
-        <div class="page-header">
-            <div class="container-fluid">
-
-
-
-
-            </div>
-        </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <!-- footer-->
-    @include('admin.footer')
-    <!-- end footer-->
 </body>
 
 </html>
