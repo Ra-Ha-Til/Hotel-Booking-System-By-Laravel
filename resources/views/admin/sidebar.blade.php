2840c1f8a59ac7a0aa00a10d1ecbea97433eeb47
sadikjj1 <islam15-5550@diu.edu.bd>
2025-07-19 19:44:19 +0600

    update

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -8,7 +8,8 @@
             <p>Web Designer</p>
         </div>
     </div>
-    <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+    <!-- Sidebar Navidation Menus-->
+    <span class="heading">Main</span>
     <ul class="list-unstyled">
         <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
 
