011ea86c9f898d68acd2d3e20d1a8dbe476c73c5
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-20 08:14:01 +0600

    home/room_details update

---
+++ resources/views/home/room_details.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- in this section css call from the home/css.blade.php -->
+    @include('home.css')
+</head>
+
+<!-- .....................body .......................-->
+
+<body class="main-layout">
+    <!-- loader  -->
+    <div class="loader_bg">
+        <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+    </div>
+    <!-- end loader -->
+
+    <!-- header -->
+    <header>
+        @include('home.header')
+    </header>
+    <!-- end header -->
+
+
+    <!--  footer -->
+    @include('home.footer')
+    <!--end footer -->
+
+</body>
+
+</html>
