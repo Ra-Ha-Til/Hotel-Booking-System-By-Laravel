98eb6da78b5ceaa9b4c8c49d26a8cb4081d6a8d6
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-20 09:08:24 +0600

    booking model create

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'room_id',
+        'name',
+        'email',
+        'phone',
+        'start_date',
+        'end_date',
+    ];
 }
