e8387a220d2296d56c55de769b54613aae276f11
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-20 08:06:33 +0600

    route updated

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ route::get('/view_room', [AdminController::class, 'view_room']);
 route::get('/room_delete/{id}', [AdminController::class, 'room_delete']);
 route::get('/room_update/{id}', [AdminController::class, 'room_update']);
 route::post('/edit_room/{id}', [AdminController::class, 'edit_room']);
+route::get('/room_details/{id}', [AdminController::class, 'room_details']);
