0263afb83584e1e0c1719823ef36a6a6a2c58922
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-20 08:11:30 +0600

    Room function call

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,8 @@ class HomeController extends Controller
 {
     public function room_details($id)
     {
+        $room = Room::find($id);
+        return view('home.room_details', compact('room'));
 
     }
 }
