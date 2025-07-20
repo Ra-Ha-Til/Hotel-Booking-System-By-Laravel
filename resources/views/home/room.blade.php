32e239561d7ffe5f57ccc2b6e6d24f0017b190ae
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-20 07:58:43 +0600

    css add

--- resources/views/home/room.blade.php
+++ resources/views/home/room.blade.php
@@ -16,11 +16,11 @@
           <div class="col-md-4 col-sm-6">
             <div id="serv_hover" class="room">
                <div class="room_img">
-                 <img style="height: 200px" width="350px" src="room/{{ $rooms->image }}" alt="#" />
+                 <img style="height: 200px; width: 350px" src="room/{{ $rooms->image }}" alt="#" />
                </div>
                <div class="bed_room">
                  <h3>{{ $rooms->room_title }}</h3>
-                 <p>I{{ $rooms->description }} </p>
+                 <p> {!! Str::limit($rooms->description, 100) !!}</p>
                </div>
             </div>
           </div>
