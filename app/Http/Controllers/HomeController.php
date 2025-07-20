c2ccb9b90f7f79954354fdedf955531cb2f10773
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-20 08:10:17 +0600

    home controller Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Room;
+
 class HomeController extends Controller
 {
-    //
+    public function room_details($id)
+    {
+
+    }
 }
