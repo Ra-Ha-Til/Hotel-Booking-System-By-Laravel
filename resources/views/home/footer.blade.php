0f4d38ab3cf769f992740edf27da7954df7934bb
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-21 13:25:09 +0600

    updated

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -6,8 +6,8 @@
                <h3>Contact US</h3>
                <ul class="conta">
                   <li><i class="fa fa-map-marker" aria-hidden="true"></i> Address</li>
-                  <li><i class="fa fa-mobile" aria-hidden="true"></i> +01 1234569540</li>
-                  <li> <i class="fa fa-envelope" aria-hidden="true"></i><a href="#"> demo@gmail.com</a></li>
+                  <li><i class="fa fa-mobile" aria-hidden="true"></i> 01533931224</li>
+                  <li> <i class="fa fa-envelope" aria-hidden="true"></i><a href="#"> rahatil880@gmail.com</a></li>
                </ul>
             </div>
             <div class="col-md-4">
